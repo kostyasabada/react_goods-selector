956236c318d8bbc253c8d094b61683656367cf82
Kostya Sabada <kostyasabada@gmail.com>
2020-09-03 12:56:04 +0300

    Updates

---
+++ precache-manifest.ccde3f348fee074008158a0c156ee093.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10d203e5d2783994775c04732914c00c",
+    "url": "./index.html"
+  },
+  {
+    "revision": "45171f9de4352ca1d2d3",
+    "url": "./static/css/main.a8436424.chunk.css"
+  },
+  {
+    "revision": "4dd0f18127de32a9553a",
+    "url": "./static/js/2.22b5d565.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.22b5d565.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "45171f9de4352ca1d2d3",
+    "url": "./static/js/main.e59e6906.chunk.js"
+  },
+  {
+    "revision": "f861d31e3669d4c8fef0",
+    "url": "./static/js/runtime-main.c970f6dd.js"
+  }
+]);
